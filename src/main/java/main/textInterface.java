6d825a639726b2a342bfa979d64fae38a66a5332
Ryan Woodzell <ryanwoodzell@gmail.com>
2024-10-25 14:39:52 -0400

    Update textInterface.java

--- src/main/java/main/textInterface.java
+++ src/main/java/main/textInterface.java
@@ -7,42 +7,51 @@ import java.util.Scanner;
 
 public class textInterface {
     public void run() throws IOException{
+        boolean runAgain = true;
+        while(runAgain) {
 
-        //import scanner to retrieve user data
-        Scanner scan = new Scanner(System.in);
+            //import scanner to retrieve user data
+            Scanner scan = new Scanner(System.in);
 
-        //create Scraper class to retrieve articles from Vox
-        Scraper scraper = new Scraper();
+            //create Scraper class to retrieve articles from Vox
+            Scraper scraper = new Scraper();
 
-        //create NLP class to run natural language processing on the text
-        NLP nlp = new NLP();
+            //create NLP class to run natural language processing on the text
+            NLP nlp = new NLP();
 
-        //retrieve the Urls from the politics page on Vox
-        scraper.parseUrls();
+            //retrieve the Urls from the politics page on Vox
+            scraper.parseUrls();
 
-        //scrape each url found using recursion
-        scraper.parseArticles();
+            //scrape each url found using recursion
+            scraper.parseArticles();
 
-        //import texts and titles
-        ArrayList<String> texts = scraper.getTexts();
-        ArrayList<String> titles = scraper.getTitles();
+            //import texts and titles
+            ArrayList<String> texts = scraper.getTexts();
+            ArrayList<String> titles = scraper.getTitles();
 
-        //User interface
-        System.out.println("Welcome to Woody's Vox Scraper! Below are the possible political articles we can analyze.");
-        System.out.println("-----------------------------------------------------------------------------------------");
+            //User interface
+            System.out.println("Welcome to Woody's Vox Scraper! Below are the possible political articles we can analyze.");
+            System.out.println("-----------------------------------------------------------------------------------------");
 
-        //print out each text and corresponding number
-        for(int i = 1; i < texts.size()+1; i++){
-            System.out.println(i + ". " + titles.get(i-1));
+            //print out each text and corresponding number
+            for (int i = 1; i < texts.size(); i++) {
+                System.out.println(i + ". " + titles.get(i - 1));
+            }
+
+            //ask which article the user would like to analyze
+            System.out.println("-----------------------------------------------------------------------------------------");
+            System.out.println("Enter the number of the article you would like to analyze: ");
+
+            //get selection
+            int selection = scan.nextInt();
+            nlp.sentimentAnalysis(texts.get(selection - 1), titles.get(selection - 1));
+
+            System.out.println("\nWould you like to continue analyzing? Y/N");
+            String answer = scan.next();
+            if (answer.equalsIgnoreCase("n")) {
+                runAgain = false;
+            }
         }
-
-        //ask which article the user would like to analyze
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println("Enter the number of the article you would like to analyze: ");
-
-        //get selection
-        int selection = scan.nextInt();
-        nlp.sentimentAnalysis(texts.get(selection - 1), titles.get(selection - 1));
-
+        System.out.println("Thank you for using my Web Scraper!");
     }
 }
